6565a66373545b715e6e5f863a9a322aa867f8fb
Ayaz Mehmood05 <ayazmehmod05@gmail.com>
2026-07-14 00:52:57 +0500

    Update image source path in Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
     >
       <div className="w-full mx-auto p-0 m-0">
         <img 
-          src="website hero image.jpg.jpeg" 
+          src="D:\shopify premium store updated version\website hero image.jpg.jpeg" 
           alt="The Ultimate Connected Habitat - Royal Sphire Premium Tech" 
           referrerPolicy="no-referrer"
           className="w-full h-auto object-cover object-center block p-0 m-0"
